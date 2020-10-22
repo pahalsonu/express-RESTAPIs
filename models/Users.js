c2183ce450f94d91b50c397df7473b29d93ecaf9
pahalsonu <pahalsonu@gmail.com>
2020-10-22 21:16:00 +0530

    User Schema Created

--- models/Users.js
+++ models/Users.js
@@ -1,25 +1,27 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// // data fields : firstname, lastname, email, (unique), password
-// const Schema = mongoose.Schema;
+const userSchema = new  Schema({
+    firstName : {
+        type : String,
+        required : true
+    },
+    lastName : {
+        type : String
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    password : {
+        type : String,
+        required : true,
+        minlength : 6
+    }
+});
 
-// const userSchema = new Schema({
-//     firstname: {
-//         type: String,
-//         required: true
-//     },
-//     lastname: {
-//         type: String
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     }
-// });
 
-// module.exports = mongoose.model("Users", userSchema, 'users_data'); 
+module.exports = mongoose.model("User", userSchema, "new_users");
+
+
